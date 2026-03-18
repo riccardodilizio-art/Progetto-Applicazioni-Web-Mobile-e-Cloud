183523f6d0ea7a1ee57a96b201572d79c678ab9b
Alberto Terenzi <a.terenzi99@gmail.com>
2026-03-18 19:10:42 +0100

    Removed hotel-site folder

--- hotel-site/src/types/Service.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react'
-
-export interface Service {
-    title: string
-    description: string
-    icon: ReactNode
-}
--- hotel-site/src/types/User.ts
+++
@@ -1,7 +0,0 @@
-export interface UserState {
-    email: string
-    role: 'admin' | 'client'
-    name?: string
-    surname?: string
-    phone?: string
-}
