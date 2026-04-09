d71d26cfb89eea9d115f7ecc02ce44a4033c983c
Riccardo <dlz.riccardo@gmail.com>
2026-04-09 10:39:14 +0200

    fix user configuration

--- Backend-hotel-site/Hotel.Site.Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ Backend-hotel-site/Hotel.Site.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -17,41 +17,31 @@ namespace Hotel.Site.Infrastructure.Persistence.Configurations
             builder.HasMany(x => x.StanzePrenotate)
                 .WithOne(x => x.User)
                 .HasForeignKey(x => x.IdUser);
-            builder.HasData(new User
-            {
-                IdUser = Guid.Parse("11111111-1111-1111-1111-111111111111"),
-                Nome = "Admin",
-                Cognome = "Hotel",
-                Email = "admin@hotelexcelsior.it",
-<<<<<<< HEAD
-                Password = "admin123",
-=======
-                Password = "$2b$11$.i42CzTY.byNwR.bvY4AM.amERMHtyRQGAhX2QtcbbvADwzW89XoG",
->>>>>>> backend
-                NumeroTelefono = "0000000000",
-                Ruolo = Role.ADMIN,
-                DataCreazione = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
-            },
-            new User
-            {
-                IdUser = Guid.Parse("22222222-2222-2222-2222-222222222222"),
-                Nome = "Mario",
-                Cognome = "Rossi",
-                Email = "cliente@hotelexcelsior.it",
-<<<<<<< HEAD
-                Password = "cliente123",
-=======
-                Password = "$2b$11$LujGaaGHK3tDqaw0A8J82.FTHz5jzj5Nwa0/bfimhXLtpqthTvIsG",
->>>>>>> backend
-                NumeroTelefono = "3331234567",
-                Ruolo = Role.CLIENT,
-                DataCreazione = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
-            }
-            );
-<<<<<<< HEAD
-=======
 
->>>>>>> backend
+            builder.HasData(
+                new User
+                {
+                    IdUser = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    Nome = "Admin",
+                    Cognome = "Hotel",
+                    Email = "admin@hotelexcelsior.it",
+                    Password = "$2b$11$.i42CzTY.byNwR.bvY4AM.amERMHtyRQGAhX2QtcbbvADwzW89XoG",
+                    NumeroTelefono = "0000000000",
+                    Ruolo = Role.ADMIN,
+                    DataCreazione = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                },
+                new User
+                {
+                    IdUser = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                    Nome = "Mario",
+                    Cognome = "Rossi",
+                    Email = "cliente@hotelexcelsior.it",
+                    Password = "$2b$11$LujGaaGHK3tDqaw0A8J82.FTHz5jzj5Nwa0/bfimhXLtpqthTvIsG",
+                    NumeroTelefono = "3331234567",
+                    Ruolo = Role.CLIENT,
+                    DataCreazione = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                }
+            );
         }
     }
 }
