a186b03e28a3ddbf9f2fe64aa617c6096e180147
Riccardo <dlz.riccardo@gmail.com>
2026-04-20 21:40:08 +0200

    fix bugs

--- Backend-hotel-site/Hotel.Site.Infrastructure/Migrations/20260420141043_AddUsersResetToken.Designer.cs
+++ Backend-hotel-site/Hotel.Site.Infrastructure/Migrations/20260420141043_AddUsersResetToken.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Hotel.Site.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hotel.Site.Infrastructure.Migrations
 {
     [DbContext(typeof(HotelSiteContext))]
-    partial class HotelSiteContextModelSnapshot : ModelSnapshot
+    [Migration("20260420141043_AddUsersResetToken")]
+    partial class AddUsersResetToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend-hotel-site/Hotel.Site.Infrastructure/Migrations/20260420141043_AddUsersResetToken.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Site.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersResetToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpiry",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResetTokenHash",
+                table: "Users",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                columns: new[] { "ResetTokenExpiry", "ResetTokenHash" },
+                values: new object[] { null, null });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                columns: new[] { "ResetTokenExpiry", "ResetTokenHash" },
+                values: new object[] { null, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ResetTokenHash",
+                table: "Users",
+                column: "ResetTokenHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ResetTokenHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpiry",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenHash",
+                table: "Users");
+        }
+    }
+}
