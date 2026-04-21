650d78059fe2177a436616ba84744d70a929828d
Riccardo <dlz.riccardo@gmail.com>
2026-04-21 16:24:11 +0200

    Fix bugs

---
+++ Backend-hotel-site/Hotel.Site.Infrastructure/Migrations/20260421141541_AddUserLockoutColumns.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Site.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserLockoutColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedLoginCount",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockoutExpiry",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "LockoutExpiry",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "IdUser",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "LockoutExpiry",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedLoginCount",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutExpiry",
+                table: "Users");
+        }
+    }
+}
